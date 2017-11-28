243aad2ed527f6927558a5f6804c7922e2570b6b
Unknown <toey.prapaporn123@gmail.com>
2017-11-28 11:29:15 +0700

    ทำเสร็จแล้ว

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
-  cout << "Mr. Anukul" << endl;
+  cout << "Miss Prapaporn Sunthornwit / BAITOEY / Wednesday 23 December 1998 / I like watch TV/ I want go to sea and forest for relax And I want sleep"
+<< endl;
   return 0;
 }
